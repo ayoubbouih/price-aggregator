b980940afdc3c8c3dd409c5948882788e59f36fd
Ismail Houhou <ismailhouhou.pro@gmail.com>
2021-05-19 13:59:30 +0100

    Scraping Button

--- aggregator/settings.py
+++ aggregator/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'aggregator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "voyageino/templates/"],
+        'DIRS': [os.path.join(BASE_DIR , "templates/"),BASE_DIR / "voyageino/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
